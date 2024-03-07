0edef7def17ae9ca56f2fe95903c8cf23fcfbe6f
lucas-gabrail <162160820+lucas-gabrail@users.noreply.github.com>
2024-03-07 02:05:58 +0000

    add more changes

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ print(x)
 y=4
 print(y)
 
-print("The sum of x and y is: ", 9)
+print("The sum of x and y is: ", x+y)
 e=7
 print(e)
-print("The sum of e and x is:  ", 12)
+print("The sum of e and x is:  ", e+x)
       
-
+h=2
+print(h)
+print("The sum of h and y is: ",  h+y)
 
