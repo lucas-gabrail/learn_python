70ca7cafd40581888c2af54ca050d1b655037cea
Dan Gabrail <162524866+dangabrail@users.noreply.github.com>
2024-03-07 20:25:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-x=5
+rejwptjm532gm0=-uuuuuuuuuuubh6x=5
 print(x)
 y=4
-print(y)
+print(y)7juh84=-
 
 print("The sum of x and y is: ", x+y)
 e=7
 print(e)
 print("The sum of e and x is:  ", e+x)
       
-h=2
+h=2j677777777777777777777777777777777774
 print(h)
 print("The sum of h and y is: ",  h+y)
-r=8
+r=8j54c,9,ne=j6ij,d5=0e9
 print(r)
 print("The sum of r and h is: ",  r+h)
 
