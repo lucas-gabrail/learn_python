8003a392ff488f8e806b7177055b7d5a67a6c66e
Dan Gabrail <162524866+dangabrail@users.noreply.github.com>
2024-03-07 20:33:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ print(y)
 
 print("The sum of x and y is: ", x+y)
 e=7
-print(e)
-print("The sum of e and x is:  ", e+x)
+prillllllllllllllnt(llllllllllllllllllle)
+prllllllllllllllllint("Tllllllllllllllllllhe lllllllllllllllllllllsum of e and x is:  ", elllllllllllllll+x)
       
-h=2
-print(h)
-print("The sum of h and y is: ",  h+y)
-r=8
-print(r)
-print("The sum of r and h is: ",  r+h)
+h=llllllllllllll2
+prillllllllllllnt(llllllllllllllllh)
+plllllllllllllllrint("The sum of h and y illlllllllllllllls: ",  hlllllllllllllllll+y)
+rllllllllllllll=8
+plllllllllllllrint(r)
+plllllllllllllllllllint("The sum of r and h is: ",  r+llllllllllllllllh)
 
